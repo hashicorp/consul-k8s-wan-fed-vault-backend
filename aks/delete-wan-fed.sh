588427abc2e8ead4e850d7558237b92cb8363109
vanphan24 <89482663+vanphan24@users.noreply.github.com>
2022-04-19 17:07:03 -0700

    Delete aks directory

--- aks/delete-wan-fed.sh
+++
@@ -1,7 +0,0 @@
-kubectl config use-context dc2
-helm delete vault-dc2
-consul-k8s uninstall -auto-approve -wipe-data   
-kubectl config use-context dc1
-helm delete vault-dc1
-consul-k8s uninstall -auto-approve -wipe-data
-
